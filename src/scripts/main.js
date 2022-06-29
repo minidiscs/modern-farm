2b2f577e7a17b8c7457a80e65d9bc80771006420
minidiscs <shanedbutler@gmail.com>
2022-06-29 15:13:12 -0400

    remove unused module import

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 import { createPlan } from "./plan.js"
-import { addPlant, usePlants } from "./field.js";
+import { usePlants } from "./field.js";
 import { plantSeeds } from "./tractor.js";
 import { harvestPlants } from "./harvester.js";
 import { catalog } from "./catalog.js";
