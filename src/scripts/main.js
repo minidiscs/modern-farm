f21dc02ed1fd581bbf9d3947a5db869d7da743c8
minidiscs <shanedbutler@gmail.com>
2022-06-29 10:38:51 -0400

    seed modules created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,18 @@
+import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js";
+import { createPotato } from "./seeds/potato.js";
+import { createWheat } from "./seeds/wheat.js";
+import { createSunflower } from "./seeds/sunflower.js";
+import { createSoybean } from "./seeds/soybean.js";
+
+
 console.log("Welcome to the main module")
 
+const yearlyPlan = createPlan()
+
+console.log(yearlyPlan)
+
+const asparagus = (createAsparagus())
+
+console.log(asparagus)
